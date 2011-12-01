aba406f298d9803e52284706f286d07d869c8bee
Aleksis Brezas <abresas@kamibu.com>
2011-12-01 16:18:30 +0000

    cleaner main

--- src/main.js
+++ src/main.js
@@ -73,58 +73,72 @@ ground.set_movable( false );
 system.addBody( ground );
 
 var carBody = null;
+var carMesh, wheel0, wheel1, wheel2, wheel3;
+var loaded = false;
 
+function onPartsLoaded() {
+    app.scene.appendChild( carMesh );
 
-app.importer.load( 'race/untitled.obj', function( carMesh ) {
-    app.importer.load( 'race/Wheel1.obj', function( wheel0 ) {
-        app.importer.load( 'race/Wheel2.obj', function( wheel1 ) {
-            app.importer.load( 'race/Wheel3.obj', function( wheel2 ) {
-                app.importer.load( 'race/Wheel4.obj', function( wheel3 ) {
-                    app.scene.appendChild( carMesh );
+    window.wheel0 = wheel0.setPosition( new Vector3( [ -0.9, -0.5,  1.35 ] ) );
+    window.wheel1 = wheel1.setPosition( new Vector3( [ 0.9, -0.5,  1.35 ] ) );
+    window.wheel2 = wheel2.setPosition( new Vector3( [ -0.9, -0.5, -1.35 ] ) );
+    window.wheel3 = wheel3.setPosition( new Vector3( [ 0.9, -0.5, -1.35 ] ) );
 
-                    window.wheel0 = wheel0.setPosition( new Vector3( [ -0.9, -0.5,  1.35 ] ) );
-                    window.wheel1 = wheel1.setPosition( new Vector3( [ 0.9, -0.5,  1.35 ] ) );
-                    window.wheel2 = wheel2.setPosition( new Vector3( [ -0.9, -0.5, -1.35 ] ) );
-                    window.wheel3 = wheel3.setPosition( new Vector3( [ 0.9, -0.5, -1.35 ] ) );
+    carMesh.appendChild( wheel0 );
+    carMesh.appendChild( wheel1 );
+    carMesh.appendChild( wheel2 );
+    carMesh.appendChild( wheel3 );
 
-                    carMesh.appendChild( wheel0 );
-                    carMesh.appendChild( wheel1 );
-                    carMesh.appendChild( wheel2 );
-                    carMesh.appendChild( wheel3 );
+    carBody = new jiglib.JCar( new FESkin3D( carMesh ) );
 
-                    carBody = new jiglib.JCar( new FESkin3D( carMesh ) );
+    var maxSteerAngle = 30;
+    var steerRate = 3;
+    var driveTorque = 100;
 
-                    var maxSteerAngle = 30;
-                    var steerRate = 3;
-                    var driveTorque = 100;
+    var width = 2;
+    var height = 1.1;
+    var depth = 4.5;
+    
+    carBody.setCar( maxSteerAngle, steerRate, driveTorque );
+    carBody.get_chassis().set_sideLengths( new Vector3D( width, height, depth, 0 ) );
+    carBody.get_chassis().moveTo( new Vector3D( 0, 5, -5 ) );
+    carBody.get_chassis().set_mass( 80 );
+    system.addBody( carBody.get_chassis() );
 
-                    var width = 2;
-                    var height = 1.1;
-                    var depth = 4.5;
-                    
-                    carBody.setCar( maxSteerAngle, steerRate, driveTorque );
-                    carBody.get_chassis().set_sideLengths( new Vector3D( width, height, depth, 0 ) );
-                    carBody.get_chassis().moveTo( new Vector3D( 0, 5, -5 ) );
-                    carBody.get_chassis().set_mass( 80 );
-                    system.addBody( carBody.get_chassis() );
+    var wheelRadius = 0.5;
+    var travel = 0.3;
+    var sideFriction = 1.7;
+    var fwdFriction = 1.5;
+    var restingFrac = 0.3;
+    var dampingFrac = 0.9;
+    var rays = 2;
+                
+    carBody.setupWheel( 0, new Vector3D( -0.9, -0.5,  1.35 ), sideFriction, fwdFriction, travel, wheelRadius, restingFrac, dampingFrac, rays );
+    carBody.setupWheel( 1, new Vector3D(  0.9, -0.5,  1.35 ), sideFriction, fwdFriction, travel, wheelRadius, restingFrac, dampingFrac, rays );
+    carBody.setupWheel( 2, new Vector3D( -0.9, -0.5, -1.35 ), sideFriction, fwdFriction, travel, wheelRadius, restingFrac, dampingFrac, rays );
+    carBody.setupWheel( 3, new Vector3D(  0.9, -0.5, -1.35 ), sideFriction, fwdFriction, travel, wheelRadius, restingFrac, dampingFrac, rays );
 
-                    var wheelRadius = 0.5;
-                    var travel = 0.3;
-                    var sideFriction = 1.7;
-                    var fwdFriction = 1.5;
-                    var restingFrac = 0.3;
-                    var dampingFrac = 0.9;
-                    var rays = 2;
-                                
-                    carBody.setupWheel( 0, new Vector3D( -0.9, -0.5,  1.35 ), sideFriction, fwdFriction, travel, wheelRadius, restingFrac, dampingFrac, rays );
-                    carBody.setupWheel( 1, new Vector3D(  0.9, -0.5,  1.35 ), sideFriction, fwdFriction, travel, wheelRadius, restingFrac, dampingFrac, rays );
-                    carBody.setupWheel( 2, new Vector3D( -0.9, -0.5, -1.35 ), sideFriction, fwdFriction, travel, wheelRadius, restingFrac, dampingFrac, rays );
-                    carBody.setupWheel( 3, new Vector3D(  0.9, -0.5, -1.35 ), sideFriction, fwdFriction, travel, wheelRadius, restingFrac, dampingFrac, rays );
-                } );
-            } );
-        } );
-    } );
-} );
+    loaded = true;
+}
+
+var w = new EventWaiter();
+app.importer.load( 'race/untitled.obj', w.callback( function( c ) {
+    console.log( c );
+    carMesh = c;
+} ) );
+app.importer.load( 'race/Wheel1.obj', w.callback( function( w ) {
+    wheel0 = w;
+} ) );
+app.importer.load( 'race/Wheel2.obj', w.callback( function( w ) {
+    wheel1 = w;
+} ) );
+app.importer.load( 'race/Wheel3.obj', w.callback( function( w ) {
+    wheel2 = w;
+} ) );
+app.importer.load( 'race/Wheel4.obj', w.callback( function( w ) {
+    wheel3 = w;
+} ) );
+w.on( 'complete', onPartsLoaded );
 
 document.onkeydown = function( e ){
     switch(e.keyCode)
@@ -171,6 +185,11 @@ document.onkeyup = function( e ){
 
 app.update = function( dt ) {
     system.integrate( dt * 0.001 );
+	
+    if ( !loaded ) {
+        return;
+    }
+
     var wheels = carBody.get_wheels();
     wheel0.position.data[ 1 ] = wheels[ 0 ].getActualPos().y;
     wheel0.setOrientation( new Quaternion().setAxisAngle( X_AXIS, wheels[ 0 ].getAxisAngle() * DEG_TO_RAD ) );
